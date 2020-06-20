734842413032155ba0380e1d87ab728a5e368a12
Colin <colin.diesh@gmail.com>
2020-06-20 19:32:14 -0400

    Experiments

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
 import React, { useState } from "react";
 import background from "./drawing.svg";
 import boneSvg from "./bone2.svg";
+import luv from "./boneluv.svg";
+import dixie from "./dixie2.svg";
+import gradient from "./gradient.svg";
 import "./App.css";
 
 function App() {
-  const [bone, setBone] = useState(false);
   return (
-    <div>
-      <Bone
-        onClick={() => {
-          setBone(true);
+    <div className="App">
+      <img
+        style={{
+          position: "absolute",
+          top: 1000,
+          left: 0,
+          width: "100%",
+          border: "1px solid black",
         }}
+        src={gradient}
       />
-      {bone ? (
-        <img
-          style={{ position: "absolute", top: 0, left: 0, width: "50%" }}
-          src={boneSvg}
-        />
-      ) : null}
+      <img src={background} style={{ width: "100%" }} />
+
+      <img
+        style={{ position: "absolute", top: 0, left: "50%", width: "50%" }}
+        src={boneSvg}
+      />
+      <img
+        style={{ position: "absolute", top: 0, left: "25%", width: "50%" }}
+        src={luv}
+      />
+      <img
+        style={{ position: "absolute", top: 100, left: 0, width: "50%" }}
+        src={dixie}
+      />
     </div>
   );
 }
 
 function Bone(props) {
   const { onClick } = props;
-  return (
-    <div className="App">
-      <img src={background} style={{ width: "100%" }} />
-      <button onClick={onClick}>Bone!!!!</button>
-    </div>
-  );
+  return <button onClick={onClick}>Bone!!!!</button>;
 }
 
 export default App;
